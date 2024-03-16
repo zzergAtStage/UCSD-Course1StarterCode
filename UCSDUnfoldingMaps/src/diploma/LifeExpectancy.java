ddeb532fc80fe00edfaa8a13a7561e99060ee236
Sergio Brü <89431263+zzergAtStage@users.noreply.github.com>
2024-03-16 20:36:57 +0100

    dev: build swing wrappers

--- UCSDUnfoldingMaps/src/diploma/LifeExpectancy.java
+++ UCSDUnfoldingMaps/src/diploma/LifeExpectancy.java
@@ -16,8 +16,7 @@ import java.util.HashMap;
 import de.fhpotsdam.unfolding.marker.Marker;
 
 /**
- * Visualizes life expectancy in different countries. 
- * 
+ * Visualizes life expectancy in different countries.
  * It loads the country shapes from a GeoJSON file via a data reader, and loads the population density values from
  * another CSV file (provided by the World Bank). The data value is encoded to transparency via a simplistic linear
  * mapping.
